1120d0f3a5a362b61dbb95e6e88410bdb3a71420
agamliel <asaph141@gmail.com>
2018-08-07 12:50:44 +0300

    refine queries

--- tableau_extension_writeback1/database.js
+++ tableau_extension_writeback1/database.js
@@ -34,14 +34,27 @@ function updateTable(pgclient,tableName)
 
 function createNewRole(pgclient,roleName)
 {
-	myQuery="CREATE TABLE " + roleName + " (	trait char(100),    stage1 int,	stage2 int,    stage3 int,	stage4 int,	stage5 int);";
+	myQuery="CREATE TABLE \
+	" + roleName + "\
+	(	trait char(100),\
+    stage1 int,\
+	stage2 int,\
+    stage3 int,\
+	stage4 int,\
+	stage5 int);";
 	runQuery(pgclient,myQuery)
 }
 
 function insertOneTrait(pgclient,roleName,traitName,valArray)
 {
 //	myQuery="INSERT INTO "+roleName+" (trait,stage1,stage2,stage3,stage4,stage5) VALUES ("+traitName+","+s1val+","+s2val+","+s3val+","+s4val+","+s5val+");";
-	myQuery="INSERT INTO "+roleName+" VALUES ("+traitName+","+String(valArray[0])+","+String(valArray[1])+","+String(valArray[2])+","+String(valArray[3])+","+String(valArray[4])+");";
+	myQuery="INSERT INTO "+roleName+" VALUES (\
+	"+traitName+"\
+	,"+String(valArray[0])+"\
+	,"+String(valArray[1])+"\
+	,"+String(valArray[2])+"\
+	,"+String(valArray[3])+"\
+	,"+String(valArray[4])+");";
     runQuery(pgclient,myQuery)
 }
 
@@ -53,7 +66,7 @@ client.connect();
 askTable(client,"asaf2");
 updateTable(client,"asaf2");
 //createNewRole(client,"asaf8");
-myvalues= new Array(1,4,8,3,9);
-insertOneTrait(client,"asaf8","'bone'",myvalues);
+myvalues= new Array(1,3,4,7,5);
+insertOneTrait(client,"asaf8","'boge'",myvalues);
 askTable(client,"asaf8");
 //client.end()
