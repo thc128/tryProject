f4516735bac3932b629ad9c7934d55924c05d302
danieldanan1 <danieldanan1@gmail.com>
2018-10-11 11:44:20 +0300

    commit before new branch some new files

---
+++ tableau_extension_writeback1/source_code_app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var products = [];
+
+app.use("/public", express.static(__dirname + "/public"));
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+app.post('/addData', function(req, res){
+  res.redirect('/addData');
+  console.log('POST request made');
+  console.log(req.body);
+  myData=JSON.parse(req.body.traits);
+  //console.log(myData.Tolerance);
+  var x
+  for (x in myData)
+  {
+	console.log(x);
+	console.log(eval('myData.'+x));
+    x_array=eval('myData.'+x);//using eval. Is it Good??????????
+    var pg = require('pg');
+    var client =new pg.Client({
+  		user: 'ddanan',
+  		host: 'rds-postgresql-10mintutorial.cwmieimhe1v4.us-east-2.rds.amazonaws.com',
+  		database: 'Testing_DB',
+  		password: 'DH204KY1!',
+  		port: 5432
+  	})
+    client.connect();
+    myQuery="INSERT INTO traits ("+x+"_Low,"+x+"_Below_Average,"+x+"_Average,"+x+"_Above_Average,"+x+"_High) VALUES (\
+  	"+x_array[0]+"\
+  	,"+x_array[1]+"\
+  	,"+x_array[2]+"\
+  	,"+x_array[3]+"\
+  	,"+x_array[4]+");";
+  	console.log(myQuery);
+      client.query(myQuery, (err, res) => {
+  	console.log("Errors: ",err)
+  	console.log("Command: ", res.command)
+  	console.log("Rows: ", res.rows)
+  	client.end()
+  })}
+});
+
+app.get('/addData', function(req, res){
+   res.render('data1.html', {products: products});
+   console.log('GET request made');
+ });
+ 
+app.listen(3000, function(){
+  console.log('Server is running on localhost:3000');
+});
