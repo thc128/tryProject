6fabb55fda815ce8caa6dff0dedd3186a043d2a8
danieldanan1 <danieldanan1@gmail.com>
2018-11-08 17:36:58 +0200

    We take the traits name from website and put them in traits from Onet.txt. Build dict in dict and get traits and score

--- onetcenter_data/connect API.py
+++ onetcenter_data/connect API.py
@@ -26,6 +26,19 @@ def get_elements(data):
     element_names=list(set(element_names))
     return element_names
 
+def get_data_values(data,dict):
+    for item in data:
+        try:
+            job_id=item[0]
+            trait=item[2]
+            data_value=item[4]
+            if not job_id in dict:
+                dict[job_id]={}
+            dict[job_id][trait]=data_value
+        except:
+            pass
+    return dict
+
 def data_mining(base_url, name):
     my_link = base_url + name + '.txt'
     r = get_data(my_link)
@@ -35,6 +48,14 @@ def data_mining(base_url, name):
 
 base_url = r'https://www.onetcenter.org/dl_files/database/db_23_0_text/'
 table_name=['Abilities','Interests','Work%20Values','Work%20Styles']
-
-
-
+big_data={}
+my_link = base_url + table_name[0] + '.txt'
+r = get_data(my_link)
+data = parse_data(r.text)
+big_data=get_data_values(data,big_data)
+print big_data
+'''
+with open(r'traits from Onet.txt',"r") as traits_pool:
+    trait_list=traits_pool.readlines()
+print trait_list
+'''
