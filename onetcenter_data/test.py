456bda9b43893bd2462245080b8f2f4b7bdc5b86
agamliel <asaph141@gmail.com>
2018-11-20 14:43:53 +0200

    The script is working, but I have no connection to the database. Added try - except for the database connections

--- onetcenter_data/test.py
+++ onetcenter_data/test.py
@@ -1,24 +1,34 @@
 import psycopg2 as pg
 import json
 
-dic_of_jobs={}
-code_to_name={}
+connection_pg=None
+pg_cursor=None
 with open(r'jsonformatter.json','r') as data:
     dic_of_jobs = json.loads(data.read())
 with open(r'codebeautify.json','r') as name_of_job:
     code_to_name = json.loads(name_of_job.read())
-
-connection_pg=pg.connect(database="Testing_DB",user="ddanan",password="DH204KY1!",host="rds-postgresql-10mintutorial.cwmieimhe1v4.us-east-2.rds.amazonaws.com",port="5432")
-pg_cursor=connection_pg.cursor()
+try:
+    connection_pg=pg.connect(database="Testing_DB",user="ddanan",password="DH204KY1!",host="rds-postgresql-10mintutorial.cwmieimhe1v4.us-east-2.rds.amazonaws.com",port="5432")
+    pg_cursor=connection_pg.cursor()
+except Exception as e:
+    print e.message
 for key, value in dic_of_jobs.iteritems():
     one_row = value.values()
     refined_job_name=code_to_name[key].replace('\'','\"')
     one_row.insert(0, "\'"+refined_job_name+"\'")
     str = ','.join(one_row)
     query="INSERT INTO ONet_traits Values("+str+")"
-    pg_cursor.execute(query)
-
+    try:
+        pg_cursor.execute(query)
+    except Exception as e:
+        print e.message
     print str
-connection_pg.commit()
+try:
+    connection_pg.commit()
+except Exception as e:
+    print e.message
 print 'looks good!'
-connection_pg.close()
+try:
+    connection_pg.close()
+except Exception as e:
+    print e.message
