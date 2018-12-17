71bd1141b06fb3c4ec7d7a7b2b7c55240e1186f9
agamliel <asaph141@gmail.com>
2018-12-17 22:30:51 +0200

    Sanitize the trait names

--- node_js_server/app.js
+++ node_js_server/app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var pg = require('pg');
 var db=require('./database_access');
 var app = express();
-
+var traitNames=['Openness','Consciousness','Extaversion','Agreeableness','Neuroticism','Secure','Anxious_preoccupied','Fearfull_Avoidant','Dissmising_Avoidant','Soical_Desirability','Creativity','Locus_of_control','Self_efficacy','Risk_taking','istress_Tolerance','Distress_Appraisal','Distress_Absorbsion','Distress_Regulation','Distress_Tolerance','Tolerance_for_Ambiguity','Ambiguous_stimuli','Complex_stimuli','Uncertain_stimuli','New_stimuli','Insoluble_stimuli','Emotional_Intelligence','Self_emotion_appraisal','Others_emotion_appraisal','Use_of_emotion','Regulation_of_emotion','Improvisation__Total_score','Improvisation_creativity_and_bricolage','Improvisasion_function_under_pressure___stress','Improvisation_spontaneity_and_persistence','Self_dicipline','The_Short_Dark_Triad','SImprovisasion_function_under_pressure___stress','Narcissism_related_tendencies','Psychopathy_related_tendencies'];
 
 //Engine images and files
 app.use(bodyParser.urlencoded({extended: true}));
@@ -48,8 +48,14 @@ app.post('/addData', async function(req, res){
 	console.log(roleData.length>0);
 	var exist = (roleData.length>0);
 	var oneTrait;
+		
 	for (oneTrait in myData)
 	{
+		if (traitNames.indexOf(oneTrait)==-1)
+		{
+			console.log("Bad request, this trait is not valid:",oneTrait);
+			break;
+		}
 		console.log(oneTrait);
 		console.log(myData[oneTrait]);
 		traitsValues=myData[oneTrait];
