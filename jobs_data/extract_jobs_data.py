fa7463c6d8db23fd90661019f872d9bd1ebb6817
Asaf <asaph141@gmail.com>
2019-03-02 23:23:50 +0200

    Updated the script.Trying to read the data from the excel file into a dictionary. Little fix in the excel file

--- jobs_data/extract_jobs_data.py
+++ jobs_data/extract_jobs_data.py
@@ -1,9 +1,13 @@
 import xlrd
 
-FILEPATH='FINAL WITH CODES Occupations Categories and sub Categories January 14 2019 (1).xls'
+FILE_PATH='FINAL WITH CODES Occupations Categories and sub Categories January 14 2019 (1).xls'
+IGNORE_SHEETS=[0,55]
+IGNORE_ROWS=[0]
 
 def read_file(file_path):
-	file_content =  xlrd.open_workbook(file_path)
+	return  xlrd.open_workbook(file_path)
+	
+	'''
 	print(file_content.sheet_names())
 	first_sheet = file_content.sheet_by_index(0)
 	categories_data={}
@@ -15,7 +19,39 @@ def read_file(file_path):
 		category_name=first_sheet.row_values(row_id)[1]
 		categories_data[category_id]=category_name
 	print(categories_data)
-	
-	
-read_file(FILEPATH)
-	
+	'''
+def create_categories_dict(my_workbook):
+	categories_data={}
+	first_sheet = my_workbook.sheet_by_index(0)
+	for row_id in range(first_sheet.nrows):
+		if row_id==0:
+			continue
+		category_id=first_sheet.row_values(row_id)[0]
+		category_name=first_sheet.row_values(row_id)[1]
+		categories_data[category_id]={'name':category_name}
+	return categories_data
+
+def parse_file(my_workbook,categories_data):
+	for sheet_id in range(my_workbook.nsheets):
+		if sheet_id in IGNORE_SHEETS:
+			continue
+		print('sheet number: '+str(sheet_id))
+		current_sheet=my_workbook.sheet_by_index(sheet_id)
+		for row_id in range(current_sheet.nrows):
+			if row_id in IGNORE_ROWS:
+				continue
+			print('row number: '+str(row_id))
+			current_row=current_sheet.row_values(row_id)
+			print(current_row)
+			job_category_id=current_row[0]
+			job_id=current_row[1]
+			job_name=current_row[2]
+			categories_data[job_category_id][job_id]=job_name
+	return categories_data
+			
+
+workbook=read_file(FILE_PATH)
+all_jobs_data=create_categories_dict(workbook)
+print(all_jobs_data)
+all_jobs_data=parse_file(workbook,all_jobs_data)	
+print(all_jobs_data)
